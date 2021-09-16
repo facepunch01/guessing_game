4178e51d53143fea9c5bf38f813148c7007ea3ad
Jake Hackl <jakerhackl@gmail.com>
2021-09-16 12:31:07 -0500

    it is done

--- target/debug/deps/guessing_game-5161db9c0484fb73.d
+++ target/debug/deps/guessing_game-5161db9c0484fb73.d
@@ -1,5 +1,5 @@
-C:\Users\Jake\Documents\guessing_game\target\debug\deps\guessing_game-5161db9c0484fb73.rmeta: src\main.rs
+c:\Users\Jake\Documents\guessing_game\target\debug\deps\guessing_game-5161db9c0484fb73.rmeta: src\main.rs
 
-C:\Users\Jake\Documents\guessing_game\target\debug\deps\guessing_game-5161db9c0484fb73.d: src\main.rs
+c:\Users\Jake\Documents\guessing_game\target\debug\deps\guessing_game-5161db9c0484fb73.d: src\main.rs
 
 src\main.rs:
